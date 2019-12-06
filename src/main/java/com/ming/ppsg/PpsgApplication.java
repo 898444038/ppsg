a1d924ecc871489d17dbb954952e2a1430a8dd86
ming <898444038@qq.com>
2019-12-06 12:09:44 +0800

    base

---
+++ src/main/java/com/ming/ppsg/PpsgApplication.java
@@ -0,0 +1,13 @@
+package com.ming.ppsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ming/ppsg/PpsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.ppsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
