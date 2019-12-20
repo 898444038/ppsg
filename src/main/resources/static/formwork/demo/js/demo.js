c08907cf1e7b4f4570003912cc5acb920034c38f
ming <898444038@qq.com>
2019-12-20 16:46:01 +0800

    superadmin2.2

--- src/main/resources/static/formwork/demo/js/demo.js
+++ src/main/resources/static/formwork/demo/js/demo.js
@@ -1,13 +1,15 @@
 'use strict';
 
 $(document).ready(function () {
+    var $body = $('body');
+
     /*--------------------------------------
         Animation
     ---------------------------------------*/
     var animationDuration;
 
 
-    $('body').on('click', '.animation-demo .card-body .btn', function(){
+    $body.on('click', '.animation-demo .card-body .btn', function(){
         var animation = $(this).text();
         var cardImg = $(this).closest('.card').find('img');
         if (animation === "hinge") {
@@ -23,5 +25,16 @@ $(document).ready(function () {
         setTimeout(function(){
             cardImg.removeClass(animation);
         }, animationDuration);
+    });
+
+
+    /*--------------------------------------
+        Icons Preview
+    ---------------------------------------*/
+    $body.on('click', '.icons-demo__item', function () {
+        var icon = $(this).find('span').text();
+
+        $('#icon-preview .icon-size > i').attr('class', 'zwicon-'+icon);
+        $('#icon-preview').modal('show');
     });
 });
